8e562fa62afb249e7c35cf5ae56e1ad1b0083024
Wenyi.Li <lwy.lwy@163.com>
2016-10-12 22:01:04 +0800

    Update unbufferdio.c
    Signed-off-by: Wenyi.Li <lwy.lwy@163.com>

--- unbufferdio.c
+++ unbufferdio.c
@@ -1,16 +1,29 @@
 #include "apue.h"
+#include <fcntl.h>
 
 #define BUFFSIZE 10 
 
 int main(void)
 {
 	int n;
+//	int fd;
 	char buf[BUFFSIZE];
 
-	while ((n = read(STDIN_FILENO, buf, BUFFSIZE)) > 0){
+/*	fd = open("test.txt", FILE_MODE);	
+	while ((n = read(fd, buf, BUFFSIZE)) > 0) {
+		if (write(STDOUT_FILENO, buf, n) != n)
+			err_sys("write error");
 		if (write(STDOUT_FILENO, buf, n) != n)
 			err_sys("write error");
 	}
+*/
+	while ((n = read(STDIN_FILENO, buf, BUFFSIZE)) > 0){
+		if (write(STDOUT_FILENO, "#", 1) != 1)
+			err_sys("write error");
+		if (write(STDOUT_FILENO, buf, n) != n)
+			err_sys("write error");
+	}
+	
 	if (n < 0)
 		err_sys("read error");
 	exit(0);
